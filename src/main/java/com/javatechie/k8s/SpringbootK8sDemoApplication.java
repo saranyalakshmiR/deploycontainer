8334f9392de1ff2e3e0c299d2c97733082f72562
saranyalakshmiR <108867173+saranyalakshmiR@users.noreply.github.com>
2022-09-14 11:23:21 +0530

    Update SpringbootK8sDemoApplication.java

--- src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringbootK8sDemoApplication {
 
 	@GetMapping("/message")
 	public String displayMessage(){
-		return "Congratulation you successfully  !!";
+		return "Hello World Hi saranya !!";
 	}
 
 	public static void main(String[] args) {
